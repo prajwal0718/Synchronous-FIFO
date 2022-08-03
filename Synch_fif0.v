f161d47cabca1b0c09b0350fa4903c047e5b9f95
Prajwal D Nayak <65390753+prajwal0718@users.noreply.github.com>
2022-08-03 10:22:04 +0530

    Update Synch_fif0.v

--- Synch_fif0.v
+++ Synch_fif0.v
@@ -1,3 +1,4 @@
+//Prajwal D Nayak.
 module sync_fifo(
  clk,
  rstn,
@@ -17,20 +18,20 @@ input clk;
 input rstn;
 input pop;
 input push;
-input [DATA_WIDTH-1:0]din;
+ input [DATA_WIDTH-1:0]din;    //writing to the memory we need din.
 
-output [DATA_WIDTH-1:0]dout;
+ output [DATA_WIDTH-1:0]dout;    //To read from the memory we need dout.
 output empty;
 output full;
-
-reg [DATA_WIDTH-1:0]fifo[DEPTH-1:0];
+// all are reg data type as they are defined inside always block.
+ reg [DATA_WIDTH-1:0]fifo[DEPTH-1:0];     //Creating the memory(RAM) to store the data.
 reg [PTR_WIDTH-1:0]rdptr, next_rdptr;
-reg [PTR_WIDTH-1:0]wrptr, next_wrptr;
+ reg [PTR_WIDTH-1:0]wrptr, next_wrptr;    // Declaring read pointer and the write pointers.
 reg [DATA_WIDTH-1:0]dout, next_dout;
 reg empty, next_empty;
 reg full, next_full;
 
-assign fullchk = push && !(|(wrptr^(rdptr-1'b1)));
+ assign fullchk = push && !(|(wrptr^(rdptr-1'b1)));  //'fullchk' to check if the last operation was write and the FIFO became full
 assign emptychk = pop && !(|(rdptr^(wrptr-1'b1)));
 
 always @ (posedge clk) //Sequential block
@@ -63,17 +64,17 @@ begin
  
   if(push)  //write
   begin
-  fifo[wrptr] = din;
-  next_wrptr  = wrptr+1;
+   fifo[wrptr] = din;   // In write data is written into memory 
+  next_wrptr  = wrptr+1;  // after writing data into memory the write pointer gets incremented.
   end
-  else if(pop)  //read
+ else if(pop)  //read 
   begin
-  next_dout    = fifo[rdptr];
-  next_rdptr   = rdptr+1;
+   next_dout    = fifo[rdptr];    // data is read from the memory using read pointer
+  next_rdptr   = rdptr+1;    // after data is read from memory the pointer also gets incremented
   end
   else
   begin
-  next_dout  = dout;
+  next_dout  = dout;    // bY DEFAULT no change it must remain same, this is given in else condition.
   next_rdptr = rdptr;
   next_wrptr = wrptr;
   end 
