53cea5ee6bcea7c41f3caedae8661e23391afe8f
Prajwal D Nayak <65390753+prajwal0718@users.noreply.github.com>
2022-08-05 17:09:44 +0530

    Update sync_fifo_tb.v

--- sync_fifo_tb.v
+++ sync_fifo_tb.v
@@ -1,81 +1,72 @@
-module Fifo_tb;
- // Inputs
- reg clk;
- reg rstn;
- reg pop;
- reg push;
- reg [7:0] din;
+`timescale 1ns / 1ps
 
- // Outputs
- wire empty;
- wire full;
- wire [7:0] dout;
+module sync_tb;
+reg [7:0] data_in;
+reg clk , rst , rd_en , wr_en;
+wire empty , full;
+wire [3:0]fifo_count;
+wire [7:0] data_out;
 
- // Instantiate the Unit Under Test (UUT)
- Fifo uut (
-  .clk(clk), 
-  .rstn(rstn), 
-  .pop(pop), 
-  .push(push), 
-  .empty(empty), 
-  .full(full), 
-  .din(din), 
-  .dout(dout)
- );
+ //Module Instatiation
+ sync_fifo DUT(data_in ,clk , rst , rd_en, wr_en, empty , full , fifo_count , data_out);
 
- always #5 clk = ~clk;  // CLOCK Generation.
+ always #5 clk = ~clk;
+  
+  task reset();
+  begin
+  clk = 1'b1;
+  rst = 1'b1;
+  rd_en = 1'b0;
+  wr_en = 1'b0;
+  data_in = 8'd0;
+  #30; rst = 1'b0;
+  end
+  endtask
  
- task reset();
- begin
- clk = 1'b1;
- rstn = 1'b0;
- pop = 1'b0;
- push = 1'b0;
- din = 8'd0;
- #30; rstn = 1'b1;
- end
- endtask
+  task read_fifo();
+  begin
+  rd_en = 1'b1;
+  #10;
+  rd_en = 1'b0;
+  end
+  endtask
+    
+  task write_fifo([7:0]din_tb);
+  begin
+  wr_en = 1'b1;
+  data_in = din_tb;
+  #10 wr_en = 1'b0;
+  end
+  endtask
+  
+  // Main code
+  initial
+  begin
+  reset();
+  #10;
+  repeat(2)
+  begin
+  write_fifo(8'h11);
+  #10;
+  write_fifo(8'h22);
+  #10;
+  write_fifo(8'h33);
+  #10;
+  write_fifo(8'h44);
+  #10;
+  end
+  #10;
+  repeat(2)
+  begin
+  read_fifo();
+  #10;
+  end
+  #10;
+  $finish;
+  end
+       
 
- task read_fifo();
- begin
- pop = 1'b1;
- #10;
- pop = 1'b0;
- end
- endtask
-   
- task write_fifo([7:0]din_tb);
- begin
- push = 1'b1;
- din = din_tb;
- #10 push = 1'b0;
- end
- endtask
- 
- // Main code
- initial
- begin
- reset();
- #10;
- repeat(2)
- begin
- write_fifo(8'h11);
- #10;
- write_fifo(8'h22);
- #10;
- write_fifo(8'h33);
- #10;
- write_fifo(8'h44);
- #10;
- end
- #10;
- repeat(2)
- begin
- read_fifo();
- #10;
- end
- #10;
- $finish;
- end
-      
+
 endmodule
+
+
